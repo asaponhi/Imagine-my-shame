98e556ebcf7626451db59df29ef8eeca1fac961f
hiroki.asayama <hiroki.asayama@i-studio.co.jp>
2022-08-18 13:38:09 +0900

    fix: refs #1 WebSocket.Server is not a constructor

--- server/index.mjs
+++ server/index.mjs
@@ -1,10 +1,18 @@
 import express from 'express'
-import ws from 'ws'
+import { WebSocketServer } from 'ws'
 
 const host = process.env.HOST || 'localhost'
 const port = process.env.PORT || 3030
+// 3
+const wss = new WebSocketServer({ port: port });
 
-const wss = new ws.Server({ port: port });
+// 2
+// import ws from 'ws'
+
+// const server = WebSocketServer.Server
+// const wss = new server({ port: port });
+// 1
+// const wss = new ws.Server({ port: port });
 const app = express()
 
 wss.on('connection', function connection(s) {
